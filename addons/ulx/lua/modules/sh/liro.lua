cfb83eb6b3823b60dc70b3d4d6f0426296b0544d
Parker Haider <parkerno552@yahoo.com>
2019-03-22 05:33:44 -0500

    Added !federal command
    - !federal <ply> <len (seconds)> <reason>

---
+++ addons/ulx/lua/modules/sh/liro.lua
@@ -0,0 +1,21 @@
+local CATEGORY_NAME = "Liro"
+
+function ulx.federal( calling_ply, target_ply, time, reason )
+
+	if reason == "" then
+		local reason = "No reason given"
+	end
+
+	for k, v in pairs( target_ply ) do
+		if not isFed( v ) then
+			grantFed( v, time, reason, calling_ply )
+		end
+	end
+
+end
+
+local federal = ulx.command( CATEGORY_NAME, "ulx federal", ulx.federal, "!federal" )
+federal:defaultAccess( ULib.ACCESS_SUPERADMIN )
+federal:addParam{ type=ULib.cmds.PlayersArg }
+federal:addParam{ type=ULib.cmds.NumArg, min=1, max=100, default=604800, hint="time", ULib.cmds.round }
+federal:addParam{ type=ULib.cmds.StringArg, hint="reason", ULib.cmds.optional, ULib.cmds.takeRestOfLine }
